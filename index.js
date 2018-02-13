4da68bc57a88e4e2e5dbd14d7c83b075d8516acc
Stanislav Sysoev <d4rkr00t@gmail.com>
2018-02-13 16:39:03 +1100

    fix: lots of small fixes

--- index.js
+++ index.js
@@ -3,35 +3,22 @@ const path = require("path");
 const promisify = require("util").promisify;
 const rimraf = promisify(require("rimraf"));
 const unlink = promisify(fs.unlink);
-const {
-  mkdirp,
-  hash,
-  fstat,
-  checksum,
-  readStream,
-  writeStream,
-  getOptsHash
-} = require("./utils");
+const { mkdirp, hash, fstat, checksum, readStream, writeStream, getOptsHash } = require("./utils");
 const CachefError = require("./error");
 
 // Write N characters per iteration, then unblock the thread
 const WRITE_CHUNK_SIZE = 500;
 
-module.exports = async function createCache(
-  opts = {
-    dir: ".cache",
-    prefix: ""
-  }
-) {
+module.exports = async function createCache(opts = {}) {
   const optsHash = getOptsHash(opts);
-  const dir = path.resolve(opts.dir);
-  const { prefix } = opts;
+  const dir = path.resolve(opts.dir || ".cache");
+  const { prefix = "" } = opts;
 
   await mkdirp(dir);
 
   return {
     async _getCacheKey(filename) {
-      const chsm = checksum(filename);
+      const chsm = await checksum(filename);
       return `${prefix}${hash(filename)}:${chsm}:${hash(optsHash)}`;
     },
 
@@ -44,21 +31,13 @@ module.exports = async function createCache(
 
       const key = await this._getCacheKey(filename);
       try {
-        return await writeStream(
-          WRITE_CHUNK_SIZE,
-          this._getCacheFileName(key),
-          value
-        );
+        return await writeStream(WRITE_CHUNK_SIZE, this._getCacheFileName(key), value);
       } catch (e) {
         try {
           await this.delete(filename);
         } catch (e) {}
 
-        throw new CachefError(
-          `Unable to add '${filename}' to cache.`,
-          e.message,
-          e.stack
-        );
+        throw new CachefError(`Unable to add '${filename}' to cache.`, e.message, e.stack);
       }
     },
 
@@ -76,11 +55,7 @@ module.exports = async function createCache(
       try {
         return await readStream(this._getCacheFileName(key));
       } catch (e) {
-        throw new CachefError(
-          `Unable to read '${filename}' from cache.`,
-          e.message,
-          e.stack
-        );
+        throw new CachefError(`Unable to read '${filename}' from cache.`, e.message, e.stack);
       }
     },
 
@@ -103,5 +78,6 @@ module.exports = async function createCache(
 // TODO: Cache key cache (inception O.o)
 // TODO: Add flow types
 // TODO: Tests
+// TODO: Clean up everything
 // TODO: Performance benchmark
 // TODO: CI
